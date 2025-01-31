b1fe3aa704057758d6a5b592a18069f038468dd3
Javi Marti <undernightcore@gmail.com>
2025-01-31 18:37:26 +0100

    feat: add delete app endpoint

--- src/routes/apps.ts
+++ src/routes/apps.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   createApp,
+  deleteApp,
   listApps,
   listenApp,
   listenAppLogs,
@@ -16,6 +17,7 @@ router.get("/:appId/realtime", listenApp);
 router.get("/:appId/logs/realtime", listenAppLogs);
 router.post("/", createApp);
 router.put("/:appId", updateApp);
+router.delete("/:appId", deleteApp);
 router.post("/:appId/start", startApp);
 router.post("/:appId/stop", stopApp);
 
