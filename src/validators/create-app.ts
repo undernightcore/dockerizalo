5e940303394e02c525a8d846fc1ee9b4f33efff7
Javi Marti <undernightcore@gmail.com>
2025-01-31 01:36:39 +0100

    feat: allow null or undefined descriptions

--- src/validators/create-app.ts
+++ src/validators/create-app.ts
@@ -1,8 +1,8 @@
-import { object, optional, string } from "zod";
+import { nullable, object, optional, string } from "zod";
 
 export const createAppValidator = object({
   name: string({ required_error: "A name is required for your app" }),
-  description: optional(string()),
+  description: optional(string()).or(nullable(string())),
   repository: string({
     required_error: "A repository is required for your app",
   }),
