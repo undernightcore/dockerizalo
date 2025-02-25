b9ec053a788cce702507c3005b25175c008b78f6
Javi Marti <undernightcore@gmail.com>
2025-02-25 21:20:25 +0100

    fix: validate app of type image

--- src/validators/app/create-image-app.ts
+++ src/validators/app/create-image-app.ts
@@ -5,25 +5,15 @@ export const createImageAppValidator = object({
     required_error: "A name is required for your app",
     invalid_type_error: "A name must be a valid string",
   }).min(1, "A name is required for your app"),
-  mode: literal("REPOSITORY", {
+  mode: literal("IMAGE", {
     required_error: "A mode for your app is required",
     invalid_type_error: "The mode must be REPOSITORY or IMAGE",
   }),
   description: optional(nullable(string())).default(null),
-  repository: string({
-    required_error: "A repository is required for your app",
-    invalid_type_error: "This repository is not a valid URL",
-  }).url("This repository is not a valid URL"),
-  branch: string({
-    required_error: "A branch is required for your app",
-    invalid_type_error: "The branch must be a valid string",
-  }),
-  image: optional(literal(null)).default(null),
-  contextPath: optional(
-    string().regex(/^(?!$)(\/(?!\.{1,2}(?:\/|$))[^\s\/]+)*\/?$/)
-  ).default("/"),
-  filePath: optional(
-    string().regex(/^(?!$)(\/(?!\.{1,2}(?:\/|$))[^\s\/]+)*\/?$/)
-  ).default("/Dockerfile"),
-  tokenId: optional(nullable(string())).default(null),
+  repository: optional(literal(null)).default(null),
+  branch: optional(literal(null)).default(null),
+  image: string({ required_error: "An image is required for your app" }),
+  contextPath: optional(literal(null)).default(null),
+  filePath: optional(literal(null)).default(null),
+  tokenId: nullable(string()),
 });
